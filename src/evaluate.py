c2d86a5dca436aca476b13b063aeb7ec9ad24187
Your Name <you@example.com>
2022-08-13 11:18:03 +0200

    further plots

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from dateutil import parser as dateparser
+
 df = pd.read_csv('speedtests.csv')
 
 downloads = df['Download']
@@ -17,5 +19,13 @@ mean_upload_mbits = uploads_in_mbits.mean().round(2)
 
 print("Mean Upload " + str(mean_upload_mbits) + " MBit/s")
 
-plt.plot(downloads_in_mbits_round)
+timestamps = df['Timestamp']
+timestamps = [dateparser.parse(time) for time in df.Timestamp]
+
+plt.plot(timestamps, downloads_in_mbits_round)
+plt.show()
+
+plt.plot(timestamps, downloads_in_mbits_round)
+plt.plot(timestamps, uploads_in_mbits_round)
+
 plt.show()
