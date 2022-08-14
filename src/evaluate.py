3a5c0302ffb289c2e8546857c1ebf95ab1bc0213
Your Name <you@example.com>
2022-08-14 12:20:46 +0200

    evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,28 +4,39 @@ import matplotlib.pyplot as plt
 from dateutil import parser as dateparser
 
 df = pd.read_csv('speedtests.csv')
+df.to_numpy()
 
-downloads = df['Download']
+downloads = df['Download'].to_numpy()
 downloads_in_mbits = downloads/1000000
 downloads_in_mbits_round = downloads_in_mbits.round(2)
 mean_download_mbits = downloads_in_mbits.mean().round(2)
 
 print("Mean Download " + str(mean_download_mbits) + " MBit/s")
 
-uploads = df['Upload']
+uploads = df['Upload'].to_numpy()
 uploads_in_mbits = uploads/1000000
 uploads_in_mbits_round = uploads_in_mbits.round(2)
 mean_upload_mbits = uploads_in_mbits.mean().round(2)
 
 print("Mean Upload " + str(mean_upload_mbits) + " MBit/s")
 
-timestamps = df['Timestamp']
-timestamps = [dateparser.parse(time) for time in df.Timestamp]
+timestamps = df['Timestamp'].to_numpy()
+timestamps = [dateparser.parse(time) for time in timestamps]
 
-plt.plot(timestamps, downloads_in_mbits_round)
+fig = plt.figure()
+ax = fig.add_subplot()
+
+ax.set_title("Download Speed")
+ax.plot(timestamps, downloads_in_mbits_round)
+ax.set_xlabel("Zeit")
+ax.set_ylabel("MBit/s")
 plt.show()
 
-plt.plot(timestamps, downloads_in_mbits_round)
-plt.plot(timestamps, uploads_in_mbits_round)
-
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_title("Up- and Download Speed")
+ax.plot(timestamps, downloads_in_mbits_round)
+ax.plot(timestamps, uploads_in_mbits_round)
+ax.set_xlabel("Zeit")
+ax.set_ylabel("MBit/s")
 plt.show()
